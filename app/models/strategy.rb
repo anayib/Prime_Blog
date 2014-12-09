f9a732e426278893f4457c8a2a740d8dbff553eb
anayib <nayib.abdala@gmail.com>
2014-12-09 15:50:55 -0500

    creates associations between models

--- app/models/strategy.rb
+++ app/models/strategy.rb
@@ -1,2 +1,4 @@
 class Strategy < ActiveRecord::Base
+	has_and_belongs_to_many :authors
+	has_many :tips
 end
--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,3 +1,3 @@
 class Tip < ActiveRecord::Base
-  belongs_to :strategy_id
+  belongs_to :strategy
 end
